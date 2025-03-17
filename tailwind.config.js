608de50fe09dc4b10c11faa79809110d63e463ee
Hassanadelani1 <luqmanadelani1@gmail.com>
2025-03-17 15:25:14 +0200

    made the acehotel section for the md and lg breakpoints

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,32 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content:["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      textStrokeWidth: {
+        DEFAULT: '1px',
+        sm: '0.5px',
+        lg: '2px',
+      },
+      textStrokeColor: {
+        DEFAULT: 'black',
+        white: 'white',
+        red: 'red',
+      },
+    },
   },
-  plugins: [],
-}
+  plugins: [
+    function ({ addUtilities }) {
+      addUtilities({
+        '.text-outline': {
+          '-webkit-text-stroke-width': '1px',
+          '-webkit-text-stroke-color': 'black',
+        },
+        '.text-outline-white': {
+          '-webkit-text-stroke-width': '1px',
+          '-webkit-text-stroke-color': 'white',
+        },
+      });
+    },
+  ],
+};
